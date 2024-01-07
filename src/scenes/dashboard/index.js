dd719df0a4f0e352b64c85940634d3554d893a62
kunal pal <kunalpal2292004@gmail.com>
2024-01-07 14:37:49 +0530

    Added the Header and Dashboard and also sidebar

--- src/scenes/dashboard/index.js
+++ src/scenes/dashboard/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import { Box } from "@mui/material";
+
+import Header from "../../components/Header";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+  return <Box display='flex' justifyContent='space-between' alignItems='center'>
+<Header title="DASHBOARD" subtitle='Welcome'/>
+  </Box>
 }
 
 export default Dashboard;
